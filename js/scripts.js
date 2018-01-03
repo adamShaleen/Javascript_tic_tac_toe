02163b2891c1013e1f52eabeb62d611abae1a018
adamShaleen <adamshaleen@gmail.com>
2018-01-02 20:55:50 -0600

    started JS, basic show and hide and some utilities to setup the game.

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,57 @@
 $(document).ready(function() {
 
-  // Defaults on load=====================
+  // Defaults on load ================================================
+  var currentGame;
   $("#gameInProgressContainer").hide();
+  $("#resetButton").hide();
 
-  // Player select
-  $("#choosePlayerContainer").click(function() {
-    $(this).hide();
-    $("#gameInProgressContainer").show();
+  // Player Select / Game create =====================================
+  $("#chooseXButton").click(function() {
+      $("#choosePlayerContainer").hide();
+      $("#gameInProgressContainer").show();
+      $("#resetButton").show();
+      currentGame = new Game("X");
   });
 
-  // Reset Game
+  $("#chooseOButton").click(function() {
+      $("#choosePlayerContainer").hide();
+      $("#gameInProgressContainer").show();
+      $("#resetButton").show();
+      currentGame = new Game("O");
+  })
+
+  // Gameplay Move ===================================================
+  $(".game-cell").click(function() {
+      addMove(currentGame, $(this).attr('id'));
+      $(this).html(currentGame.player);
+      console.log(currentGame.moves);
+  });
+
+  // Reset Game ======================================================
   $("#resetButton").click(function() {
     $("#gameInProgressContainer").hide();
+    $("#resetButton").hide();
     $("#choosePlayerContainer").show();
+    deleteGame(currentGame);
   });
+
+  // Utilities =======================================================
+
+  function Game(player) {
+      this.player = player;
+      this.moves = [];
+      this.gameOver = false;
+  }
+
+  function addMove(game, move) {
+    game.moves.push(move);
+  }
+
+  function deleteGame(game) {
+    delete game.player;
+    delete game.moves;
+    delete game.gameOver;
+    $(".game-cell").html("");
+  }
 
 });
